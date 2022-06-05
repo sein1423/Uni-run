32d00119358e85b40ecac2c5267b6cc190dd0aca
sein1423 <kun3650@gmail.com>
2022-06-06 02:29:12 +0900

    Initial commit

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+// 발판을 생성하고 주기적으로 재배치하는 스크립트
+public class Item : MonoBehaviour
+{
+    public GameObject StarPrefab, BombPrefab, AddJumpPrefab; // 생성할 발판의 원본 프리팹
+    public int count = 3; // 생성할 발판의 개수
+
+    public float timeBetSpawnMin = 1.25f; // 다음 배치까지의 시간 간격 최솟값
+    public float timeBetSpawnMax = 2.25f; // 다음 배치까지의 시간 간격 최댓값
+    private float delta; // 다음 배치까지의 시간 간격
+    private float Spawntime;
+
+
+
+    void Start()
+    {
+        Spawntime = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+        delta = 0f;
+    }
+
+    void Update()
+    {
+        // 순서를 돌아가며 주기적으로 발판을 배치
+        if (GameManager.instance.isGameover) return;
+
+        delta += Time.deltaTime;
+        if (Spawntime < delta)
+        {
+            delta = 0f;
+            Spawntime = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+
+            int itemprobability = Random.Range(1, 11);
+            if (itemprobability == 1)
+            {
+                GameObject Itemtype = Instantiate(BombPrefab);
+                Itemtype.transform.position = new Vector2(12f, Random.Range(0f, 4f));
+            }
+            else if(itemprobability == 2)
+            {
+                GameObject Itemtype = Instantiate(StarPrefab);
+                Itemtype.transform.position = new Vector2(12f, Random.Range(0f, 4f));
+            }
+            else
+            {
+                GameObject Itemtype = Instantiate(AddJumpPrefab);
+                Itemtype.transform.position = new Vector2(12f, Random.Range(0f, 4f));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+// PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
+public class PlayerController : MonoBehaviour {
+   public AudioClip deathClip; // 사망시 재생할 오디오 클립
+   public float jumpForce = 700f; // 점프 힘
+
+   private int jumpCount = 0; // 누적 점프 횟수
+   private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+   private bool isDead = false; // 사망 상태
+   public bool isGetStar = false;
+
+   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+   private Animator animator; // 사용할 애니메이터 컴포넌트
+   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+
+   private void Start() {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+   private void Update() {
+        if (isDead) return;
+
+        if(Input.GetMouseButtonDown(0) && jumpCount < 2)
+        {
+            jumpCount++;
+            playerRigidbody.velocity = Vector2.zero;
+            playerRigidbody.AddForce(Vector2.up * jumpForce);
+            playerAudio.Play();
+        }
+        else if(Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0f)
+        {
+            playerRigidbody.velocity *= .5f;
+        }
+        animator.SetBool("Grounded", isGrounded);
+   }
+
+   private void Die() {
+        if (!isGetStar)
+        {
+
+            animator.SetTrigger("Die");
+            playerAudio.clip = deathClip;
+            playerAudio.Play();
+
+            playerRigidbody.velocity = Vector2.zero;
+            isDead = true;
+
+            GameManager.instance.OnPlayerDead();
+
+            playerRigidbody.isKinematic = true;
+        }
+
+   }
+
+   private void OnTriggerEnter2D(Collider2D other) {
+        if (!isDead)
+        {
+            if (other.tag == "Dead" && !isGetStar)
+            {
+                Die();
+            }
+            else if (other.tag == "Item")
+            {
+                Destroy(other.gameObject);
+                isGetStar = true;
+            }
+            else
+            {
+                Destroy(other.gameObject);
+                jumpCount--;
+            }
+        }
+   }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Dead") && isGetStar)
+        {
+            isGetStar = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+       if(collision.contacts[0].normal.y > .7f)//고양이할때 천장에 머리박으면 점프뛰는거 해소해주는 코드
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
+   }
+
+   private void OnCollisionExit2D(Collision2D collision) {
+        isGrounded = false;
+   }
+}
